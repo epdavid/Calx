de05b0fe2cd0133bc4037bad889c623337339d3b
Evan David <evandavid@evans-macbook-pro.local>
2018-08-01 15:48:00 -0400

    Initial Commit

---
+++ Calx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calx
+//
+//  Created by Evan David on 8/1/18.
+//  Copyright © 2018 Evan David. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Calx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calx
+//
+//  Created by Evan David on 8/1/18.
+//  Copyright © 2018 Evan David. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
