225abd235d2fd383eb169548e87ca893c507e1a0
Evan David <evan.david@hotmail.com>
2018-08-02 12:18:42 -0400

    Successfully got font to resize

--- Calx/ViewController.swift
+++ Calx/ViewController.swift
@@ -9,12 +9,37 @@
 import Cocoa
 import MathParser
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextFieldDelegate {
     var parenthAccum:Int = 0 //Parentheses accumulator for matching up closed parentheses
+    let evaluator:Evaluator = Evaluator()
+    
+    override func controlTextDidChange(_ obj: Notification) {
+        print("working")
+        
+        if (resultField.stringValue.count >= 10 && resultField.font == NSFont.systemFont(ofSize: 56)) {
+            resultField.font = NSFont.systemFont(ofSize: 28)
+        }
+        else if (resultField.stringValue.count >= 18 && resultField.font == NSFont.systemFont(ofSize: 28)) {
+            resultField.font = NSFont.systemFont(ofSize: 14)
+        }
+        else if (resultField.stringValue.count >= 36 && resultField.font == NSFont.systemFont(ofSize: 14)) {
+            resultField.font = NSFont.systemFont(ofSize: 8)
+        }
+        else if (resultField.stringValue.count <= 36 && resultField.font == NSFont.systemFont(ofSize: 8)) {
+            resultField.font = NSFont.systemFont(ofSize: 14)
+        }
+        else if (resultField.stringValue.count <= 18 && resultField.font == NSFont.systemFont(ofSize: 14)) {
+            resultField.font = NSFont.systemFont(ofSize: 28)
+        }
+        else if (resultField.stringValue.count <= 10 && resultField.font == NSFont.systemFont(ofSize: 28)) {
+            resultField.font = NSFont.systemFont(ofSize: 56)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        resultField.maximumNumberOfLines = 1
+        resultField.delegate = self as NSTextFieldDelegate
     }
 
     override var representedObject: Any? {
@@ -24,7 +49,7 @@ class ViewController: NSViewController {
     }
     
     func checkOperator() -> Bool {
-        if (resultField.stringValue.suffix(3) == " + " || resultField.stringValue.suffix(3) == " - " || resultField.stringValue.suffix(3) == " * " || resultField.stringValue.suffix(3) == " / ") {
+        if (resultFieldMut.suffix(3) == " + " || resultFieldMut.suffix(3) == " - " || resultFieldMut.suffix(3) == " * " || resultFieldMut.suffix(3) == " / ") {
             return true
         }
         else {
@@ -33,151 +58,183 @@ class ViewController: NSViewController {
     }
     
 // ____________CALCULATOR BUTTONS_____________
+    var resultFieldMut:String = "0" {
+        didSet {
+            print("working")
+            resultField.stringValue = resultFieldMut
+            
+            if (resultField.stringValue.count >= 10 && resultField.font == NSFont.systemFont(ofSize: 56)) {
+                resultField.font = NSFont.systemFont(ofSize: 28)
+            }
+            else if (resultField.stringValue.count >= 18 && resultField.font == NSFont.systemFont(ofSize: 28)) {
+                resultField.font = NSFont.systemFont(ofSize: 14)
+            }
+            else if (resultField.stringValue.count >= 36 && resultField.font == NSFont.systemFont(ofSize: 14)) {
+                resultField.font = NSFont.systemFont(ofSize: 8)
+            }
+            else if (resultField.stringValue.count <= 36 && resultField.font == NSFont.systemFont(ofSize: 8)) {
+                resultField.font = NSFont.systemFont(ofSize: 14)
+            }
+            else if (resultField.stringValue.count <= 18 && resultField.font == NSFont.systemFont(ofSize: 14)) {
+                resultField.font = NSFont.systemFont(ofSize: 28)
+            }
+            else if (resultField.stringValue.count <= 10 && resultField.font == NSFont.systemFont(ofSize: 28)) {
+                resultField.font = NSFont.systemFont(ofSize: 56)
+            }
+        }
+    }
+    
     @IBOutlet var resultField: NSTextField!
     
     
     @IBAction func calcDel(_ sender: Any) {
         if checkOperator() {
-            resultField.stringValue.removeLast(3)
+            resultFieldMut.removeLast(3)
         }
-        else if (resultField.stringValue != "0" && resultField.stringValue.count > 1) {
-            resultField.stringValue.removeLast()
+        else if (resultFieldMut != "0" && resultFieldMut.count > 1) {
+            resultFieldMut.removeLast()
         }
-        else if (resultField.stringValue.count == 1) {
-            resultField.stringValue = "0"
+        else if (resultFieldMut.count == 1) {
+            resultFieldMut = "0"
         }
     }
     
     @IBAction func calcPlus(_ sender: Any) {
-        if (resultField.stringValue != "0" && !checkOperator()) {
-            resultField.stringValue += " + "
+        if (resultFieldMut != "0" && !checkOperator()) {
+            resultFieldMut += " + "
         }
     }
     @IBAction func calcMinus(_ sender: Any) {
         //This one is going to be tricky--think about the logic of it
+        resultFieldMut += " mom"
+        
     }
     @IBAction func calcMult(_ sender: Any) {
-        if (resultField.stringValue != "0" && !checkOperator()) {
-            resultField.stringValue += " * "
+        if (resultFieldMut != "0" && !checkOperator()) {
+            resultFieldMut += " * "
         }
     }
     @IBAction func calcDivide(_ sender: Any) {
-        if (resultField.stringValue != "0" && !checkOperator()) {
-            resultField.stringValue += " / "
+        if (resultFieldMut != "0" && !checkOperator()) {
+            resultFieldMut += " / "
         }
     }
     
     @IBAction func calculate(_ sender: Any) {
-        let expression = NSExpression(format: resultField.stringValue)
-        let result = expression.expressionValue(with: nil, context: nil) as? Double
-        resultField.stringValue = "\(result ?? 0)"
+        do {
+            let expression = try Expression (string: resultFieldMut)
+            let value = try evaluator.evaluate(expression)
+            resultFieldMut = "\(value)"
+        }
+        catch {}
     }
     
     @IBAction func calcDot(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "."
+        if (resultFieldMut == "0") {
+            resultFieldMut = "."
         }
         else {
-            resultField.stringValue += "."
+            resultFieldMut += "."
         }
     }
     @IBAction func calc0(_ sender: Any) {
-        if (resultField.stringValue != "0") {
-            resultField.stringValue += "0"
+        if (resultFieldMut != "0") {
+            resultFieldMut += "0"
         }
     }
+   
     @IBAction func calc1(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "1"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "1"
         }
         else {
-            resultField.stringValue += "1"
+            resultFieldMut += "1"
         }
     }
     @IBAction func calc2(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "2"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "2"
         }
         else {
-            resultField.stringValue += "2"
+            resultFieldMut += "2"
         }
     }
     @IBAction func calc3(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "3"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "3"
         }
         else {
-            resultField.stringValue += "3"
+            resultFieldMut += "3"
         }
     }
     @IBAction func calc4(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "4"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "4"
         }
         else {
-            resultField.stringValue += "4"
+            resultFieldMut += "4"
         }
     }
     @IBAction func calc5(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "5"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "5"
         }
         else {
-            resultField.stringValue += "5"
+            resultFieldMut += "5"
         }
     }
     @IBAction func calc6(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "6"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "6"
         }
         else {
-            resultField.stringValue += "6"
+            resultFieldMut += "6"
         }
     }
     @IBAction func calc7(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "7"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "7"
         }
         else {
-            resultField.stringValue += "7"
+            resultFieldMut += "7"
         }
     }
     @IBAction func calc8(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "8"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "8"
         }
         else {
-            resultField.stringValue += "8"
+            resultFieldMut += "8"
         }
     }
     @IBAction func calc9(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "9"
+        if (resultFieldMut == "0") {
+            resultFieldMut = "9"
         }
         else {
-            resultField.stringValue += "9"
+            resultFieldMut += "9"
         }
     }
     @IBAction func calcOpenP(_ sender: Any) {
-        if (resultField.stringValue == "0") {
-            resultField.stringValue = "("
+        if (resultFieldMut == "0") {
+            resultFieldMut = "("
             parenthAccum += 1
         }
         else {
-            resultField.stringValue += "("
+            resultFieldMut += "("
             parenthAccum += 1
         }
     }
     @IBAction func calcCloseP(_ sender: Any) {
         if (parenthAccum != 0) {
-            resultField.stringValue += ")"
+            resultFieldMut += ")"
             parenthAccum -= 1
         }
         
     }
     
     @IBAction func calcClear(_ sender: Any) {
-        resultField.stringValue = "0"
+        resultFieldMut = "0"
     }
     
     
